423aa87c300b4349d0c875babc122b577bee8673
haydeng21 <hayden@coilapp.com>
2017-08-09 17:55:43 -0500

    finished available locations model. Began uploading guides to the model.

--- final-project/final-project/AvailableLocationsModel.swift
+++ final-project/final-project/AvailableLocationsModel.swift
@@ -28,13 +28,12 @@ class AvailableLocations {
     var delegate: AvailableLocationsModelListenerProtocol?
     
     var locations: [location] = [
-        
+        // USA
         location(cityCommaCountry: "New York, USA", coordinate: CLLocationCoordinate2D(latitude: 40.730610, longitude: -73.935242)),
         location(cityCommaCountry: "Boston, USA", coordinate: CLLocationCoordinate2D(latitude: 42.364506, longitude: -71.038887)),
+        // Mexico
         location(cityCommaCountry: "Cabo, Mexico", coordinate: CLLocationCoordinate2D(latitude: 22.887201, longitude: -109.905038)),
-        location(cityCommaCountry: "Nassau, Bahamas", coordinate: CLLocationCoordinate2D(latitude: 25.034280, longitude: -77.396280)),
-        location(cityCommaCountry: "Los Angeles, USA", coordinate: CLLocationCoordinate2D(latitude: 34.052234, longitude: -118.243685)),
-        location(cityCommaCountry: "Barcelona, Spain", coordinate: CLLocationCoordinate2D(latitude: 41.390205, longitude: 2.154007)),/*
+        // SE Asia
         location(cityCommaCountry: "Bangkok, Thailand", coordinate: CLLocationCoordinate2D(latitude: 13.756331, longitude: 100.501765)),
         location(cityCommaCountry: "Chiang Mai, Thailand", coordinate: CLLocationCoordinate2D(latitude: 18.786626, longitude: 98.983727)),
         location(cityCommaCountry: "Pai, Thailand", coordinate: CLLocationCoordinate2D(latitude: 19.358219, longitude: 98.440486)),
@@ -50,7 +49,29 @@ class AvailableLocations {
         location(cityCommaCountry: "Hoi An, Vietnam", coordinate: CLLocationCoordinate2D(latitude: 15.880058, longitude: 108.338047)),
         location(cityCommaCountry: "Ho Chi Minh, Vietnam", coordinate: CLLocationCoordinate2D(latitude: 10.823099, longitude: 106.629664)),
         location(cityCommaCountry: "Hong Kong, China", coordinate: CLLocationCoordinate2D(latitude: 22.283835, longitude: 114.173355)),
-        location(cityCommaCountry: "El Nido, Phillipines", coordinate: CLLocationCoordinate2D(latitude: 11.209709, longitude: 119.462254)),*/
+        location(cityCommaCountry: "El Nido, Phillipines", coordinate: CLLocationCoordinate2D(latitude: 11.209709, longitude: 119.462254)),
+        // Africa
+        location(cityCommaCountry: "Marrakesh, Morocco", coordinate: CLLocationCoordinate2D(latitude: 31.631794, longitude: -7.989261)),
+        // Europe
+         location(cityCommaCountry: "Barcelona, Spain", coordinate: CLLocationCoordinate2D(latitude: 41.390205, longitude: 2.154007)),
+         location(cityCommaCountry: "Interlaken, Switzerland", coordinate: CLLocationCoordinate2D(latitude: 46.686348, longitude: 7.863205)),
+         location(cityCommaCountry: "Prague, Czech Republic", coordinate: CLLocationCoordinate2D(latitude: 50.075538, longitude: 14.437800)),
+         location(cityCommaCountry: "Berlin, Germany", coordinate: CLLocationCoordinate2D(latitude: 52.520007, longitude: 13.404954)),
+         location(cityCommaCountry: "Paris, France", coordinate: CLLocationCoordinate2D(latitude: 48.856614, longitude: 2.352222)),
+         location(cityCommaCountry: "Nice, France", coordinate: CLLocationCoordinate2D(latitude: 43.710173, longitude: 7.261953)),
+         location(cityCommaCountry: "Dublin, Ireland", coordinate: CLLocationCoordinate2D(latitude: 53.349805, longitude: -6.260310)),
+         location(cityCommaCountry: "Budapest, Hungary", coordinate: CLLocationCoordinate2D(latitude: 47.497912, longitude: 19.040235)),
+         location(cityCommaCountry: "Florence, Italy", coordinate: CLLocationCoordinate2D(latitude: 43.769560, longitude: 11.255814)),
+         location(cityCommaCountry: "Amalfi Coast, Italy", coordinate: CLLocationCoordinate2D(latitude: 40.634003, longitude: 14.602681)),
+         location(cityCommaCountry: "Lagos, Portugal", coordinate: CLLocationCoordinate2D(latitude: 37.102788, longitude: -8.673027)),
+         location(cityCommaCountry: "Ibiza, Spain", coordinate: CLLocationCoordinate2D(latitude: 38.906914, longitude: 1.432343)),
+         location(cityCommaCountry: "Munich, Germany", coordinate: CLLocationCoordinate2D(latitude: 48.135125, longitude: 11.581980)),
+         location(cityCommaCountry: "Amsterdam, Netherlands", coordinate: CLLocationCoordinate2D(latitude: 52.370216, longitude: 4.895168)),
+         location(cityCommaCountry: "Athens, Greece", coordinate: CLLocationCoordinate2D(latitude: 37.983810, longitude: 23.727539)),
+         location(cityCommaCountry: "Santorini, Greece", coordinate: CLLocationCoordinate2D(latitude: 36.393156, longitude: 25.461509)),
+         location(cityCommaCountry: "Milan, Italy", coordinate: CLLocationCoordinate2D(latitude: 45.464204, longitude: 9.189982)),
+         location(cityCommaCountry: "Rome, Italy", coordinate: CLLocationCoordinate2D(latitude: 41.902783, longitude: 12.496366)),
+         location(cityCommaCountry: "Sicily, Italy", coordinate: CLLocationCoordinate2D(latitude: 37.599994, longitude: 14.015356))
            ]
     
     func numLocations() -> Int {
